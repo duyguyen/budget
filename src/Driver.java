a18fc7e0d1d7ae655f03a5b06180ceebca18bb53
Thanh <duy8855@gmail.com>
2018-12-06 12:04:36 -0600

    Add .gitignore

--- src/Driver.java
+++ src/Driver.java
@@ -2,13 +2,12 @@ import items.ItemFactory;
 
 public class Driver {
     public static void main(String[] args) {
-//        FinancialTrack financialTrack = new FinancialTrack(new ItemFactory());
-//
-//        financialTrack.run();
+        FinancialTrack financialTrack = new FinancialTrack(new ItemFactory());
+
+        financialTrack.run();
 
 //        Date date = new Date();
 //        System.out.println(date.toString());
-        System.out.println("Hello World");
     }
 }
 
